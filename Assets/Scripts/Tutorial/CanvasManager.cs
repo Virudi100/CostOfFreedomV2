b8d0275f3b6034c71d4888974a7ced4134631333
IsMingo <97234500+IsMingo@users.noreply.github.com>
2022-09-29 13:52:42 +0200

    zone tuto updated

--- Assets/Scripts/Tutorial/CanvasManager.cs
+++ Assets/Scripts/Tutorial/CanvasManager.cs
@@ -7,13 +7,17 @@ public class CanvasManager : MonoBehaviour
 {
     public GameObject lookUp;
     public GameObject lookDown;
+    public GameObject controllerPicture;
+    public GameObject rightTriggerText;
     private float _canvasCountdown;
     private float currCountdownValue;
     private float currCountdownValueBis;
+    private float currCountdownValueTris;
     private void Start()
     {
         StartCoroutine(StartCountdown());
         StartCoroutine(StartCountdownBis());
+        StartCoroutine(StartCountdownTris());
     }
 
     public IEnumerator StartCountdown(float countdownvalue = 10)
@@ -40,6 +44,21 @@ public class CanvasManager : MonoBehaviour
             currCountdownValueBis--;
         }
         lookDown.gameObject.SetActive(false);
+        controllerPicture.gameObject.SetActive(true);
+        rightTriggerText.gameObject.SetActive(true);
+    }
+    
+    public IEnumerator StartCountdownTris(float countdownvalueTris = 30)
+    {
+        currCountdownValueTris = countdownvalueTris;
+        while (currCountdownValueTris > 0)
+        {
+            Debug.Log("countdown " + currCountdownValueTris);
+            yield return new WaitForSeconds(1.0f);
+            currCountdownValueTris--;
+        }
+        controllerPicture.gameObject.SetActive(false);
+        rightTriggerText.gameObject.SetActive(true);
     }
 
     // Update is called once per frame
